f7eeb829b199e18a47bfb95b96be8e213743d79e
Jayson <jayson.dupuis60@gmail.com>
2023-11-20 18:09:16 +0100

    Avancee du header

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text}) => {
   return(
-    <button className="px-4 py-0.5 bg-grey rounded-2xl text-white">{text}</button>
+    <button className="px-5 py-1.5 bg-grey rounded-3xl text-white">{text}</button>
   )
 }
 

01b05b393e69ef38c6f40de321a1ed769fbbdd1c
Adonisse <adonis.chahine@etud.u-picardie.fr>
2023-11-21 15:43:03 +0100

    recup_questions

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,13 @@ const URL = url;
 const { MongoClient } = require ('mongodb');
 const client = new MongoClient(URL);
 
-function recup_questions(){
-
+async function recup_questions(db, theme){
+    const collection = db.collection(theme);
+    try{
+        const questions = await collection.findOne({libelle: theme});
+    } catch(e){ throw e; }
+    
+    return questions;
 }
 
 async function main(){

2ea6292a46d3bbdd523c710f82dc2da33cb477a7
Adonisse <adonis.chahine@etud.u-picardie.fr>
2023-11-21 11:52:06 +0100

    base de liens avec bdd

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const URL = url;
+
+const { MongoClient } = require ('mongodb');
+const client = new MongoClient(URL);
+
+function recup_questions(){
+
+}
+
+
+async function main(){
+    await client.connect();
+    const db = client.db('nomdb');
+}
+
+main()
+    .catch(console.error)
+    .finally(()=> client.close());

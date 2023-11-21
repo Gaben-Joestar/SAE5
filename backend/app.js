e6898aed186340062ab0e9a343da6e6a7936da9f
Adonisse <adonis.chahine@etud.u-picardie.fr>
2023-11-21 16:00:34 +0100

    recup_questions

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,26 @@
 const URL = url;
 
+const nomdb = 'loupepastonquizz';
+
 const { MongoClient } = require ('mongodb');
 const client = new MongoClient(URL);
+
+async function main(){
+    await client.connect();
+    const db = client.db(nomdb);
+}
+
+main()
+    .catch(console.error)
+    .finally(()=> client.close());
+
 
 async function recup_questions(db, theme){
     const collection = db.collection(theme);
     try{
         const questions = await collection.findOne({libelle: theme});
     } catch(e){ throw e; }
-    
-    return questions;
+
+    return tojson(questions);
 }
 
-async function main(){
-    await client.connect();
-    const db = client.db('nomdb');
-}
-
-main()
-    .catch(console.error)
-    .finally(()=> client.close());

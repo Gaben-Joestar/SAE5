5ddd072af800e9464996a642357ca2b309ee48ec
Gabin-Talbot <97462081+Gaben-Joestar@users.noreply.github.com>
2023-11-22 13:57:38 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Connexion from "./pages/Connexion";
-import Home from "./pages/Home";
-import Inscription from "./pages/Inscription";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Connexion from './pages/Connexion';
+import Home from './pages/Home';
+import Inscription from './pages/Inscription';
+import Partie from './pages/Partie';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/inscription" element={<Inscription />} />
-        <Route path="*" element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/connexion' element={<Connexion />} />
+        <Route path='/partie' element={<Partie />} />
+        <Route path='/inscription' element={<Inscription />} />
+        <Route path='*' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );

6baa56ca1725d6cecb240469f435c7bc0e2272f1
Gaben-Joestar <gabot7904@gmail.com>
2023-11-22 18:57:22 +0100

    Update InscriptionPage2.js
    ajout "afficher mot de passe"

--- frontend/src/components/InscriptionPage2.js
+++ frontend/src/components/InscriptionPage2.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const InscriptionPage2 = () => {
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [password2, setPassword2] = useState('');
+    const [showPassword2, setShowPassword2] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
+    const togglePassword2Visibility = () => {
+        setShowPassword2(!showPassword2);
+    }
+
     return (
         <div className='flex flex-col align-middle ml-auto mr-auto'>
             <div className='flex flex-col items-baseline mt-12  gap-4'>
                 <div className='flex flex-col'>
                     <label htmlFor="password" className='inter-medium'>Choisissez votre mot de passe :</label>
-                    <input type="password" name="password" id="password" required className="rounded-2xl drop-shadow-md w-128 py-1 pl-3" />
+                    <input type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" required className="rounded-2xl drop-shadow-md w-128 py-1 pl-3" />
+                    <div className='flex flex-row gap-1 pl-4 mt-1'>
+                        <input type="checkbox" checked={showPassword} onChange={togglePasswordVisibility} name='afficherMdp' id='afficherMdp' />
+                        <label htmlFor="afficherMdp">Afficher le mot de passe</label>
+
+                    </div>
                 </div>
                 <div className='flex flex-col'>
                     <label htmlFor="passwordConfirm" className='inter-medium'>Confirmer votre mot de passe :</label>
-                    <input type="passwordConfirm" name="passwordConfirm" id="passwordConfirm" required className="rounded-2xl drop-shadow-md w-128 py-1 pl-3" />
+                    <input type={showPassword2 ? 'text' : 'password'} value={password2} onChange={(e) => setPassword2(e.target.value)} name="passwordConfirm" id="passwordConfirm" required className="rounded-2xl drop-shadow-md w-128 py-1 pl-3" />
+                    <div className='flex flex-row gap-1 pl-4 mt-1'>
+                        <input type="checkbox" checked={showPassword2} onChange={togglePassword2Visibility} name='afficherMdpConfirmation' id='afficherMdpConfirmation' />
+                        <label htmlFor="afficherMdpConfirmation">Afficher le mot de passe</label>
+
+                    </div>
+
                 </div>
             </div>
             <div>

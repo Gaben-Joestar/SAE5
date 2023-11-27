1206feb69fce0f73183cbf4be858d45f3e4105d7
Gaben-Joestar <gabot7904@gmail.com>
2023-11-27 15:10:48 +0100

    Modification ProgressBar
    Ajout image et transition

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ProgressBar = ({ percentage }) => {
     return (
         <div className='progress-bar' style={{ '--progress': `${percentage}%` }}>
+            <img src="./img/loupe-offre-gold.png" alt="Progression" className='w-1/6' />
         </div>
     );
 };

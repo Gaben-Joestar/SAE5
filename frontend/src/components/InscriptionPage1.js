0e79a2d7cb55d963feeef0b370fca5e5a6c54e42
Gaben-Joestar <gabot7904@gmail.com>
2024-01-02 22:58:15 +0100

    tests Validité InscriptionPage1

--- frontend/src/components/InscriptionPage1.js
+++ frontend/src/components/InscriptionPage1.js
@@ -1,29 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const InscriptionPage1 = ({ onButtonClick }) => {
+    const [email, setEmail] = useState('');
+    const [pseudo, setPseudo] = useState('');
+    const [dateNaissance, setDateNaissance] = useState('');
+
+    const [emailError, setEmailError] = useState('');
+    const [pseudoError, setPseudoError] = useState('');
+    const [dateNaissanceError, setDateNaissanceError] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleEmailBlur = () => {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailPattern.test(email)) {
+            setEmailError('Adresse email invalide');
+        } else {
+            setEmailError('');
+        }
+    }
+
+    const handlePseudoChange = (e) => {
+        setPseudo(e.target.value);
+    }
+
+    const handlePseudoBlur = () => {
+        // Ajoutez votre logique de validation pour le pseudo ici
+        if (pseudo.length < 3) {
+            setPseudoError('Le pseudo doit contenir au moins 3 caractères');
+        } else {
+            setPseudoError('');
+        }
+    }
+
+    const handleDateNaissanceChange = (e) => {
+        setDateNaissance(e.target.value);
+    }
+
+    const handleDateNaissanceBlur = () => {
+        // Ajoutez votre logique de validation pour la date de naissance ici
+        // Par exemple, vérifier si la date est dans un format spécifique, etc.
+        if (!dateNaissance) {
+            setDateNaissanceError('Veuillez entrer une date de naissance');
+        } else {
+            setDateNaissanceError('');
+        }
+    }
+
+    const isNextButtonDisabled = !!emailError || !!pseudoError || !!dateNaissanceError || !email || !pseudo || !dateNaissance;
+
+
     return (
-        <div className='flex flex-col align-middle ml-auto mr-auto' >
-            <div className='flex flex-col items-baseline mt-12  gap-4'>
+        <div className='flex flex-col align-middle ml-auto mr-auto'>
+            <div className='flex flex-col items-baseline mt-12 gap-4'>
                 <div className='flex flex-col'>
                     <label htmlFor="email" className='inter-medium'>Email</label>
-                    <input type="email" name="email" id="email" required className="rounded-2xl drop-shadow-md w-128 py-1 pl-3" />
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        required
+                        value={email}
+                        onChange={handleEmailChange}
+                        onBlur={handleEmailBlur}
+                        className="rounded-2xl drop-shadow-md w-128 py-1 pl-3"
+                    />
+                    {emailError && <p className=" text-red pl-3">{emailError}</p>}
                 </div>
                 <div className='flex flex-col'>
                     <label htmlFor="pseudo" className='inter-medium'>Pseudo</label>
-                    <input type="text" name="pseudo" id="pseudo" required className="rounded-2xl drop-shadow-md w-128 py-1 pl-3" />
+                    <input
+                        type="text"
+                        name="pseudo"
+                        id="pseudo"
+                        required
+                        value={pseudo}
+                        onChange={handlePseudoChange}
+                        onBlur={handlePseudoBlur}
+                        className="rounded-2xl drop-shadow-md w-128 py-1 pl-3"
+                    />
+                    {pseudoError && <p className="text-red pl-3">{pseudoError}</p>}
                 </div>
                 <div className='flex flex-col items-start'>
                     <label htmlFor="dateNaissance" className='inter-semi-bold'>Date de Naissance</label>
-                    <input type="date" name="dateNaissance" id="dateNaissance" className="rounded-2xl drop-shadow-md w-25 py-1 pl-3" />
+                    <input
+                        type="date"
+                        name="dateNaissance"
+                        id="dateNaissance"
+                        required
+                        value={dateNaissance}
+                        onChange={handleDateNaissanceChange}
+                        onBlur={handleDateNaissanceBlur}
+                        className="rounded-2xl drop-shadow-md w-25 py-1 pl-3"
+                    />
+                    {dateNaissanceError && <p className="text-red pl-3">{dateNaissanceError}</p>}
                 </div>
-
             </div>
             <div className='flex flex-col justify-center align-middle mt-7'>
                 <div className='grow-0 ml-auto '>
-                    <button className="px-5 py-1.5 bg-grey rounded-3xl text-white" onClick={onButtonClick}>Suivant</button>
+                    <button className="px-5 py-1.5 bg-grey rounded-3xl text-white" onClick={onButtonClick} disabled={isNextButtonDisabled}>Suivant</button>
                 </div>
             </div>
-
         </div>
     );
 };

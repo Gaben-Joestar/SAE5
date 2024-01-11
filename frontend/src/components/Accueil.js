2af9dc8205a2817a386ca8f183ed581256669ebb
Gaben-Joestar <gabot7904@gmail.com>
2024-01-11 15:35:15 +0100

    Modification Taille Logo

--- frontend/src/components/Accueil.js
+++ frontend/src/components/Accueil.js
@@ -4,7 +4,7 @@ import Button from './Button';
 const Accueil = () => {
     return (
         <div className="flex flex-col justify-center items-center ">
-            <img src='./img/grand-logo.PNG' alt="logo" className='w-1/3' />
+            <img src='./img/grand-logo.PNG' alt="logo" className='w-2-7' />
             <div className='text-center inter-semi-bold text-light-grey'>
                 <p>Le jeu de quizz en ligne qui rend l'apprentissage amusant pour tous.</p>
                 <p>Rejoins-nous et défie tes connaissances ! 🌟📚🤓</p>

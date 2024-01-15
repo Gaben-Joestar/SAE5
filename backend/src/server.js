55f864cc87ceb4a832c7f9958b29a0aa5d02c684
Adonisse <adonis.chahine@etud.u-picardie.fr>
2024-01-15 21:48:46 +0100

    changes

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const quizzRoutes = require('./routes/quizzRoutes');
+const userRoutes = require('./routes/userRoutes');
+const payRoutes = require('./routes/payRoutes');
+const dbConnectionMiddleware = require('./middlewares/dbConnectionMiddleware');
+
+app.use(express.json());
+app.use(dbConnectionMiddleware);
+app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+app.use('/quizz', quizzRoutes);
+app.use('/user', userRoutes);
+app.use('/yunuspay', payRoutes)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
